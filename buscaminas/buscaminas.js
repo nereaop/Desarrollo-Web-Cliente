b5782a1bc7cdb2df56d5b865f61ad44428999269
nereaop <nereagamermh@gmail.com>
2022-10-11 19:49:06 +0200

    cambios buenos

--- buscaminas/buscaminas.js
+++ buscaminas/buscaminas.js
@@ -37,31 +37,39 @@ let posColumna;
 
 
 while (contadorMinas < numMinas) {
-    posFila = Math.floor(Math.random()*maxFilas);
-    posColumna = Math.floor(Math.random()*maxColumnas);
+    posFila = Math.floor(Math.random() * maxFilas);
+    posColumna = Math.floor(Math.random() * maxColumnas);
 
     if (arrayTablero[posFila][posColumna] != 'MINA') {
         arrayTablero[posFila][posColumna] = 'MINA';
-        contadorMinas++ ;
+        contadorMinas++;
     };
-  };
+};
 
 
-let numMinasAlrededor = 0;
-let columna=0;
+let numMinasAlrededor;
 
 for (let fila = 0; fila < maxFilas; fila++) {
-    for (let cFila = fila - 1; cFila < fila + 1; cFila++) {
-        for (let cColumna = columna - 1; cColumna < columna + 1; cColumna++) {
-            if (cFila > 0 && cFila < maxFilas && cColumna > 0 && cColumna < maxColumnas) {
-                console.log(arrayTablero[cFila][cColumna]);
-                if (arrayTablero[cFila][cColumna] == 'MINA') {
-                    numMinasAlrededor++;
+    for (let columna = 0; columna < maxColumnas; columna++) {
+        numMinasAlrededor = 0;
+        if (arrayTablero[fila][columna] != 'MINA') {
+            for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                if (cFila >= 0 && cFila < maxFilas) {
+                    for (let cColumna = columna - 1; cColumna < columna + 1; cColumna++) {
+                        if ((cFila >= 0 && cFila < maxFilas) && (cColumna >= 0 && cColumna < maxColumnas)) {
+                            if (arrayTablero[cFila][cColumna] == 'MINA') {
+
+                                numMinasAlrededor++;
+                            }
+                        }
+                    }
+
                 }
-            } 
+            }
+            arrayTablero[fila][columna] = numMinasAlrededor;
         }
+
     }
-    arrayTablero[fila][columna] = numMinasAlrededor;
 }
 
 
