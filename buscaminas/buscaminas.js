06becff1a6ce4799424b90e0bd1f05801029cf14
nereaop <nereagamermh@gmail.com>
2022-10-17 16:27:53 +0200

    error solucionado

--- buscaminas/buscaminas.js
+++ buscaminas/buscaminas.js
@@ -1,85 +1,91 @@
-let maxFilas = prompt('¿Cuántas filas quieres?');
-let maxColumnas = prompt('¿Cuántas columnas quieres?');
-let numMinas = prompt('¿Cuántas minas quieres introducir?');
-
-let arrayTablero = [];
-
-// Creamos el tablero en html
-function pintarTablero(tablero, filas, columnas) {
+function AppFuncionando() {
+    // Se piden los datos al usuario
+        let maxFilas = prompt('¿Cuántas filas quieres?');
+        let maxColumnas = prompt('¿Cuántas columnas quieres?');
+        let numMinas = prompt('¿Cuántas minas quieres introducir?');
     
-
-    document.write('<table>');
-
-    for (let i = 0; i < maxFilas; i++) {
-        document.write('<tr>');
-
-        for (let j = 0; j < maxColumnas; j++) {
-            document.write('<td>', arrayTablero[i][j], '</td>');
+        tableroBuscaminas = crearArrayTablero(maxFilas, maxColumnas);
+        tableroBuscaminas = colocarMinas(tableroBuscaminas, numMinas, maxFilas, maxColumnas);
+        tableroBuscaminas = contarMinas (tableroBuscaminas, maxFilas, maxColumnas)
+        pintarTablero(tableroBuscaminas, maxFilas, maxColumnas);
+    }
+        
+    
+    
+    function crearArrayTablero(maxFilas, maxColumnas) {
+    // Crear array bidimensional para guardar las minas
+        let arrayTablero = [];
+    
+        for (let fila = 0; fila < maxFilas; fila++) {
+            arrayTablero[fila] = new Array(maxColumnas);
+    
+            for (let columna = 0; columna < maxColumnas; columna++) {
+                arrayTablero[fila][columna] = '';
+            }
         }
-
-        document.write('</tr>');
+    
+        return arrayTablero
     }
-    document.write('</table>');
-};
-// Crear array bidimensional para guardar las minas
-function crearArray(filaMaximas, columnasMaximas){
-
-
-
-for (let fila = 0; fila < maxFilas; fila++) {
-    arrayTablero[fila] = new Array(maxColumnas);
-
-    for (let columna = 0; columna < maxColumnas; columna++) {
-        arrayTablero[fila][columna] = '';
-    }
-}
-};
-
-//Colorcar las minas
-function colocarMinas(numeroMinas) {
-    let posFila;
-    let posColumna;
-    let contadorMinas = 0;
-
-    while (contadorMinas < numMinas) {
-        posFila = Math.floor(Math.random() * maxFilas);
-        posColumna = Math.floor(Math.random() * maxColumnas);
-
-        if (arrayTablero[posFila][posColumna] != 'MINA') {
-            arrayTablero[posFila][posColumna] = 'MINA';
-            contadorMinas++;
+    
+    function colocarMinas (arrayTablero, numMinas,maxFilas, maxColumnas) {
+        let contadorMinas = 0;
+        let posFila;
+        let posColumna;
+    
+    
+        while (contadorMinas < numMinas) {
+            posFila = Math.floor(Math.random() * maxFilas);
+            posColumna = Math.floor(Math.random() * maxColumnas);
+    
+            if (arrayTablero[posFila][posColumna] != 'MINA') {
+                arrayTablero[posFila][posColumna] = 'MINA';
+                contadorMinas++;
+            };
         };
-    };
-};
-
-function buscarMinas(tablero, filaMaximas, columnasMaximas) {
-    let numMinasAlrededor;
-
-    for (let fila = 0; fila < maxFilas; fila++) {
-        for (let columna = 0; columna < maxColumnas; columna++) {
-            numMinasAlrededor = 0;
-            if (arrayTablero[fila][columna] != 'MINA') {
-                for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
-                    if (cFila >= 0 && cFila < maxFilas) {
-                        for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
-                            if ((cFila >= 0 && cFila < maxFilas) && (cColumna >= 0 && cColumna < maxColumnas)) {
-                                if (arrayTablero[cFila][cColumna] == 'MINA') {
-
+    
+        return arrayTablero
+    }
+    
+    function contarMinas (arrayTablero, maxFilas, maxColumnas) {
+        let numMinasAlrededor;
+    
+        for (let fila = 0; fila < maxFilas; fila++) {
+            for (let columna = 0; columna < maxColumnas; columna++) {
+                numMinasAlrededor = 0;
+                if (arrayTablero[fila][columna] != 'MINA') {
+                    for (let cFila = fila - 1; cFila <= fila + 1; cFila++) {
+                        if (cFila >= 0 && cFila < maxFilas) {
+                            for (let cColumna = columna - 1; cColumna <= columna + 1; cColumna++) {
+                                if (cColumna >= 0 && cColumna < maxColumnas &&
+                                    arrayTablero[cFila][cColumna] == 'MINA') {
                                     numMinasAlrededor++;
                                 }
                             }
                         }
-
+                        arrayTablero[fila][columna] = numMinasAlrededor;
                     }
+        
                 }
-                arrayTablero[fila][columna] = numMinasAlrededor;
             }
-
         }
+        return arrayTablero
     }
-};
-
-pintarTablero(arrayTablero, maxFilas, maxColumnas);
-crearArray(maxFilas, maxColumnas);
-colocarMinas(numMinas);
-buscarMinas(arrayTablero, maxColumnas, maxFilas);
+    
+    function pintarTablero(tablero, filas, columnas) {
+    // Creamos el tablero en html
+        document.write('<table>');
+    
+        for (let i = 0; i < filas; i++) {
+            document.write('<tr>');
+        
+            for (let j = 0; j < columnas; j++) {
+                document.write('<td>' + tablero[i][j] + '</td>');
+            }
+        
+            document.write('</tr>');
+        }
+        document.write('</table>');
+    
+    }
+    
+    AppFuncionando();
