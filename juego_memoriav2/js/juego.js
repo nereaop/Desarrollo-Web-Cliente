c84dab68aedf52d40262f81a7d62cfa210333980
nereaop <nereagamermh@gmail.com>
2023-01-19 18:21:58 +0100

    funcion si son pareja finalizada

--- juego_memoriav2/js/juego.js
+++ juego_memoriav2/js/juego.js
@@ -5,7 +5,10 @@ class Tablero {
         this.casillas = this.filas * this.columnas;
         this.emoticonos = ["<p>&#128512;</p>", "<p>&#128055;</p>", "<p>&#128047;</p>", "<p>&#128536;</p>", "<p>&#128526;</p>", "<p>&#128518;</p>", "<p>&#128519;</p>", "<p>&#128521;</p>", "<p>&#128525;</p>", "<p>&#128514;</p>"];
 
+        this.par = false;
 
+        this.preguntaUsuario();
+        this.comprobacionCasillas();
         this.crearTablero();
         this.colocarEmojis();
 
@@ -23,6 +26,31 @@ class Tablero {
             }
         }
         console.log(this.arrayTablero);
+    }
+
+
+    preguntaUsuario() {
+        this.filas = prompt('¿Cuantas filas quieres?');
+        this.columnas = prompt('¿Cuantas columnas quieres?');
+        this.casillas = this.filas * this.columnas;
+    }
+
+    comprobacionCasillas() {
+
+        if (this.casillas % 2 == 0) {
+            this.dibujarTableroDOM();
+        }
+
+        while (this.casillas % 2 != 0 || this.casillas < 4) {
+            this.par = false;
+            window.alert("Ha ocurrido un error, has introducido numeros los cuales las casillas son impar. Por favor, introduce filas y columnas cuyas casillas sean par");
+            this.casillas = 0;
+            this.preguntaUsuario();
+            if (this.casillas % 2 == 0) {
+                this.dibujarTableroDOM();
+                this.par = true;
+            }
+        }
     }
 
     dibujarTableroDOM() {
@@ -80,47 +108,21 @@ class Tablero {
     }
 }
 
-class Memorin {
-    constructor() {
-        this.preguntaUsuario();
-        this.casillas = this.filas * this.columnas;
-        this.tablero = new Tablero(this.filas, this.columnas);
-        this.comprobacionCasillas();
+class Memorin extends Tablero {
+    constructor(filas, columnas) {
+        super(filas, columnas);
+        // this.tablero = new Tablero(this.filas, this.columnas);
+        this.carta1 = "";
+        this.carta2 = "";
+        this.carta1Pos = "";
+        this.contador = 0;
+
         this.añadirListeners();
-    }
-    preguntaUsuario() {
-        this.filas = prompt('¿Cuantas filas quieres?');
-        this.columnas = prompt('¿Cuantas columnas quieres?');
-    }
-
-    crearTableroJuego() {
-        this.tablero = new Tablero(this.filas, this.columnas);
-    }
-
-
-    comprobacionCasillas() {
-
-        if (this.casillas % 2 != 0) {
-            let par = false;
-            while (par == false) {
-                window.alert("Ha ocurrido un error, has introducido numeros los cuales las casillas son impar. Por favor, introduce filas y columnas cuyas casillas sean par");
-                this.preguntaUsuario();
-                this.casillas = 0;
-                if (this.casillas % 2 == 0) {
-                    this.crearTableroJuego();
-                    par = true;
-                }
-            }
-
-        }
-        
-        if (this.casillas % 2 == 0) {
-            this.crearTableroJuego();
-        }   
     }
 
     añadirListeners() {
         let celda;
+
         for (let i = 0; i < this.filas; i++) {
             for (let j = 0; j < this.columnas; j++) {
                 celda = document.getElementById(`f${i}_c${j}`);
@@ -128,61 +130,47 @@ class Memorin {
             }
         }
     }
+
     despejar(eventos) {
         let evento = eventos || window.event;
         let celda = evento.currentTarget;
 
         this.despejarCelda(celda);
     }
+
     despejarCelda(celda) {
+        this.contador++;
         let simboloCarta = "<p>✺</p>";
-        let carta1;
-        let carta2;
         let fila = parseInt(celda.dataset.fila);
         let columna = parseInt(celda.dataset.columna);
-        let valorCelda = this.tablero.arrayTablero[fila][columna];
+        let valorCelda = this.arrayTablero[fila][columna];
 
-        let contador = 1;
-        celda.removeEventListener('contextmenu', this.despejarCelda.bind(this));
-        celda.oncontextmenu = function () {
-            contador++;
+        if (this.contador == 1) {
+            celda.style.background = "white";
+            this.carta1 = valorCelda;
+            this.carta1Pos = celda;
+            celda.innerHTML = this.carta1;
+        } else if (this.contador == 2) {
+            this.carta2 = valorCelda;
+            celda.innerHTML = this.carta2;
+            celda.style.background = "white";
+            this.contador = 0;
+            if (this.carta1 == this.carta2) {
+                celda.style.background = "green";
+                this.carta1Pos.style.background = "green";
+                celda.innerHTML = this.carta2;
+                celda.removeEventListener('contextmenu', this.despejarCelda.bind(this));
+                this.carta1Pos.removeEventListener('contextmenu', this.despejarCelda.bind(this));
+            } else if (this.carta1 != this.carta2) {
+                setTimeout(() => {
+                    celda.style.background = "#8affdc";
+                    celda.innerHTML = simboloCarta;
+                    this.carta1Pos.style.background = "#8affdc";
+                    this.carta1Pos.innerHTML = simboloCarta;
+                }, 2000);
+            }
         }
-        console.log(contador);
-
-        if (contador == 1) {
-            celda.style.backgroundColor = "white";
-            carta1 = valorCelda;
-            celda.innerHTML = carta1;
-            console.log(carta1);
-        } else if (contador == 2) {
-            carta2 = valorCelda;
-            celda.innerHTML = carta2;
-            celda.style.backgroundColor = "white";
-            console.log(carta2);
-        } else if (carta1 == carta2) {
-            celda.style.backgroundColor = "green";
-            celda.innerHTML = carta2;
-        } else if (carta1 != carta2) {
-            setTimeout(function () {
-                celda.style.backgroundColor = "#8affdc";
-            }, 3000);
-            setTimeout(function () {
-                celda.innerHTML = simboloCarta;
-                ;
-            }, 3000);
-        }
-
-
-
-
-
-
-
-
-
     }
-
-
 
 
 }
